0b18eef90a1e102a28879ea69693314668d9c326
Andrea Cano <andreacanogisbert@gmail.com>
2022-11-23 14:18:55 -0800

    finish clock

---
+++ exercise.js
@@ -0,0 +1,50 @@
+class Clock {
+    constructor() {
+        let time =  new Date();
+        this.hours = time.getHours();
+        this.seconds = time.getSeconds();
+        this.minutes = time.getMinutes();
+
+        const boundTick = this._tick.bind(this) // function-style
+        setInterval(boundTick, 1000);
+    }
+
+    printTime() {
+        let outputTime = `${this.hours}: ${this.minutes}: ${this.seconds}`;
+        console.log(outputTime)
+    }
+
+    _tick() {
+        this.incrementSeconds();
+        this.printTime();
+    }
+
+    incrementSeconds() {
+        this.seconds++;
+
+        if (this.seconds === 60) {
+            this.seconds = 0;
+            this.incrementMinutes();
+        }
+    }
+
+    incrementMinutes() {
+        this.minutes++;
+
+        if (this.minutes === 60) {
+            this.minutes = 0;
+            this.incrementHours();
+        }
+    }
+
+    incrementHours() {
+        this.hours++;
+
+        if (this.hours === 24) {
+            this.hours = 0;
+        }
+    }
+    
+}
+
+const clock = new Clock();
